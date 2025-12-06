45d0eeee2420626e76cec59bf0fd66d5b8cf6ba8
anas3952 <aalghamdi3952@stu.kau.edu.sa>
2025-12-07 00:39:04 +0300

    Setup Assignment 2: Adeed Views,Model, and basic structure

---
+++ CTF_Platform_V2/CTF_Platform_V2/Models/Challenge.cs
@@ -0,0 +1,9 @@
+﻿namespace CTF_Platform_V2.Models
+{
+    public class Challenge
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Points { get; set; }
+    }
+}
