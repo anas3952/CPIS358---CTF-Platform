e74c1edace7449bb04cb6a4bedee942fd4fb9091
anas3952 <aalghamdi3952@stu.kau.edu.sa>
2025-12-07 00:52:04 +0300

    Fix: Remove duplicate folders

--- CTF_Platform_V2/CTF_Platform_V2/Models/Challenge.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CTF_Platform_V2.Models
-{
-    public class Challenge
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public int Points { get; set; }
-    }
-}
--- CTF_Platform_V2/CTF_Platform_V2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CTF_Platform_V2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
